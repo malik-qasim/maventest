f383e9cd8f596e2392e829a65a4ec1c4c1dd9add
Qasim <qasim@qasim.com>
2022-11-28 12:34:47 +0500

    Maven Test Project

---
+++ src/test/java/MathLib.java
@@ -0,0 +1,14 @@
+public class MathLib {
+	public int add(int a, int b) {
+		return a+b;
+	}
+	public double add(double a, double b) {
+		return a+b;
+	}
+	public int mul(int a, int b) {
+		return a*b;
+	}
+	public double mul(double a, double b) {
+		return a*b;
+	}
+}
